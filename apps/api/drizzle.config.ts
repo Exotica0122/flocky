2d997afd5582188ebc73b85ae555b012884922cd
Peter An <anpeteran2@gmail.com>
2026-02-13 16:08:20 +1300

    fix: update database URL from process in drizzle config

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,13 +2,11 @@ import "dotenv/config";
 
 import { defineConfig } from "drizzle-kit";
 
-import { env } from "@/env.js";
-
 export default defineConfig({
   out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL!, // TODO: Make this work with env.ts
   },
 });
