454f27ffc1c75ad7cbbadf5a5583ad42d0955422
Peter An <anpeteran2@gmail.com>
2026-02-13 17:08:19 +1300

    refactor: update home page to index

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -1,7 +1,7 @@
 import { index, layout, prefix, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  index("routes/index.tsx"),
   ...prefix("app", [
     layout("components/layout/app-layout.tsx", [
       route("dashboard", "routes/app/dashboard.tsx"),
